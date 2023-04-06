456ea5ff885caa1e6debe2efc854809513b9e0dd
Huseyin Taze <76104888+huseyinfreshyy@users.noreply.github.com>
2023-04-07 00:29:19 +0300

    CRUD operations added for ProductManager

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -7,4 +7,7 @@ public interface IProductService
     List<Product> GetAll();
     List<Product> GetAllByCategoryId(int id);
     List<Product> GetByUnitPrice(decimal min, decimal max);
+    void Add(Product product);
+    void Delete(Product product);
+    void Update(Product product);
 }
