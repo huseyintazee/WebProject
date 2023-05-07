0f7ef61590a88d1d0f1a245be7e1c7cd6ede3f80
Huseyin Taze <76104888+huseyinfreshyy@users.noreply.github.com>
2023-05-07 15:12:34 +0300

    ProductController code refactoring and added requests

--- Business/Concrete/ProductManager.cs
+++ Business/Concrete/ProductManager.cs
@@ -19,7 +19,7 @@ public class ProductManager : IProductService
 
     public IDataResult<List<Product>> GetAll()
     {
-        if (DateTime.Now.Hour == 22)
+        if (DateTime.Now.Hour == 1)
             return new ErrorDataResult<List<Product>>(Messages.MaintenanceTime);
 
         return new SuccessDataResult<List<Product>>(_productDal.GetAll(), Messages.ProductsListed);
