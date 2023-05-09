1ae0b8694361ef9b855df83ff4f34a5b9987ba27
Huseyin Taze <76104888+huseyinfreshyy@users.noreply.github.com>
2023-05-09 14:58:24 +0300

    fix

--- Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ Core/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Core.DataAccess.EntityFramework;
 
-public class EfEntityRepositoryBase<TEntity, TContext>:IEntityRepository<TEntity>
+public class EfEntityRepositoryBase<TEntity, TContext> : IEntityRepository<TEntity>
     where TEntity : class, IEntity, new()
     where TContext : DbContext, new()
 {
